ca64f7d160f474fabf0a04313a05bee716b818e9
Jean-Pierre Fouche <director@sharpit.co.za>
2022-02-12 10:32:06 +0000

    in progress

--- q5-practice-exam-001/terraform/modules/q5/vpc_central_target_group.tf
+++ q5-practice-exam-001/terraform/modules/q5/vpc_central_target_group.tf
@@ -3,4 +3,5 @@ resource "aws_lb_target_group" "vpc_central_target_group" {
   port     = 443
   protocol = "HTTPS"
   vpc_id   = aws_vpc.vpc_central.id
+  target_type = "ip"
 }
--- q5-practice-exam-001/terraform/modules/q5/vpc_central_target_group_attachment.tf
+++ q5-practice-exam-001/terraform/modules/q5/vpc_central_target_group_attachment.tf
@@ -1,11 +1,12 @@
-//resource "aws_lb_target_group_attachment" "vpc_central_target_group_attachment_a" {
-//  for_each = data.aws_network_interface.vpc_central_eni_a
-//  target_group_arn = aws_lb_target_group.vpc_a_target_group.arn
-//  target_id        = each.value.private_ip
-//  port             = 443
-//}
-//
-//data "aws_network_interface" "vpc_central_eni_a" {
-//  for_each = aws_vpc_endpoint.vpc_endpoint_a
-//  id       = each.value
-//}
+resource "aws_lb_target_group_attachment" "vpc_central_target_group_attachment_a" {
+  for_each          = data.aws_network_interface.vpc_central_a_enis
+  target_group_arn  = aws_lb_target_group.vpc_a_target_group.arn
+  target_id         = each.value.private_ip
+  availability_zone = var.vpc_central_availablility_zone_a
+  port              = 443
+}
+
+data aws_network_interface "vpc_central_a_enis" {
+  for_each = aws_vpc_endpoint.vpc_endpoint_a.network_interface_ids
+  id       = each.value
+}
